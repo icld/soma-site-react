867fd989ea31ef9768b94e9c4ddcee7dcbe980af
icld <ian@icldesign.com>
2021-05-25 17:16:41 -0700

    deleted unused imports

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 // import { useEffect } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
