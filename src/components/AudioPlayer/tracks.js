ab35eed3fc69ce7df9d8e001afeb8f53a0d91d2a
icld <ian@icldesign.com>
2021-05-26 15:09:22 -0700

    add cover to player and temp audio

--- src/components/AudioPlayer/tracks.js
+++ src/components/AudioPlayer/tracks.js
@@ -1,48 +1,53 @@
-import imgSrc from "./assets/mcentire-face.jpeg";
+//stereolab
+import imgStLab from "./assets/stereoLab-cover.jpeg";
+import trcStLab from "./assets/icld2.mp3";
+//yolatengo
+import trcYoLa from "./assets/Project.mp3";
+import imgYoLa from "./assets/yolatengo-cover.jpg";
+//bss
 import imgBSS from "./assets/Bss-forgiveness-cover.jpeg";
-import smplTrck from "./assets/file_example_WAV_10MG.wav";
+import trcBSS from "./assets/icld1.m4a";
+//Ryley
+import imgRyley from "./assets/ryley-cover.jpg";
+import trcRyley from "./assets/icld1.m4a";
+//teenage
+import imgTngFn from "./assets/tnage-cover.jpg";
+import trcTngFn from "./assets/icld1.m4a";
 
 const tracks = [
   {
     title: "Miss Modular",
     artist: "Stereolab",
-    audioSrc: smplTrck,
-    image: imgSrc,
+    audioSrc: trcStLab,
+    image: imgStLab,
     color: "",
   },
   {
     title: "For You Too",
     artist: "Yo La Tengo",
-    audioSrc: smplTrck,
-    image: imgSrc,
+    audioSrc: trcYoLa,
+    image: imgYoLa,
     color: "",
   },
   {
     title: "Sweetest Kill",
     artist: "Broken Social Scene",
-    audioSrc: smplTrck,
+    audioSrc: trcBSS,
     image: imgBSS,
     color: "",
   },
   {
     title: "Rang Dizzy",
     artist: "Ryley Walker",
-    audioSrc: smplTrck,
-    image: imgSrc,
+    audioSrc: trcRyley,
+    image: imgRyley,
     color: "",
   },
   {
     title: "Time Stops",
     artist: "Teenage Fanclub",
-    audioSrc: smplTrck,
-    image: imgSrc,
-    color: "",
-  },
-  {
-    title: "For You Too",
-    artist: "Yo La Tengo",
-    audioSrc: smplTrck,
-    image: imgSrc,
+    audioSrc: trcTngFn,
+    image: imgTngFn,
     color: "",
   },
 ];
